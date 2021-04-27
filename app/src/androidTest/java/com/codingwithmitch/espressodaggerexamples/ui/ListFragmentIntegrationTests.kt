5684ce29a2d2dcdd0349a6cf8efe03a66edd197e
guopeng li <guopeng.li@technicolor.com>
2021-04-27 10:09:49 +0200

    Accessing Activity Instance

--- app/src/androidTest/java/com/codingwithmitch/espressodaggerexamples/ui/ListFragmentIntegrationTests.kt
+++ app/src/androidTest/java/com/codingwithmitch/espressodaggerexamples/ui/ListFragmentIntegrationTests.kt
@@ -1,7 +1,10 @@
 package com.codingwithmitch.espressodaggerexamples.ui
 
+import androidx.appcompat.widget.Toolbar
+import androidx.lifecycle.Observer
 import androidx.test.core.app.launchActivity
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.doesNotExist
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
@@ -25,11 +28,6 @@ class ListFragmentIntegrationTests :BaseMainActivityTest(){
 
     @get: Rule
     val espressoIdlingResourceRule = EspressoIdlingResourceRule()
-
-
-    override fun injectTest(application: TestBaseApplication) {
-        (application.appComponent as TestAppComponent).inject(this)
-    }
 
     @Test
     fun isBLogListEmpty(){
@@ -56,7 +54,56 @@ class ListFragmentIntegrationTests :BaseMainActivityTest(){
 
         onView(withId(R.id.no_data_textview))
             .check(matches(withEffectiveVisibility(Visibility.VISIBLE)))
-
     }
+
+    @Test
+    fun isCategoryListEmpty(){
+        val app = InstrumentationRegistry
+            .getInstrumentation()
+            .targetContext
+            .applicationContext as TestBaseApplication
+
+        val apiService = configureFakeApiService(
+            blogsDataSource = Constants.BLOG_POSTS_DATA_FILENAME,
+            categoriesDataSource = Constants.EMPTY_LIST,
+            networkDelay = 0L,
+            application = app
+        )
+
+        configureFakeRepository(apiService, app)
+        injectTest(app)
+
+        val scenario = launchActivity<MainActivity>().onActivity{ mainActivity ->
+            val toolbar: Toolbar = mainActivity.findViewById(R.id.tool_bar)
+
+            // wait for the job to complete to open the menu
+            mainActivity.viewModel.viewState.observe(mainActivity, Observer { viewState->
+                if(viewState.activeJobCounter.size == 0){
+                    toolbar.showOverflowMenu()
+                }
+            })
+        }
+
+        // assert
+        onView(withSubstring("earthporn"))
+            .check(doesNotExist())
+
+        onView(withSubstring("dogs"))
+            .check(doesNotExist())
+
+        onView(withSubstring("fun"))
+            .check(doesNotExist())
+
+        onView(withSubstring("All"))
+            .check(matches(isDisplayed()))
+    }
+
+
+
+    override fun injectTest(application: TestBaseApplication) {
+        (application.appComponent as TestAppComponent).inject(this)
+    }
+
+
 
 }
